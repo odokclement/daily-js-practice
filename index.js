84081c08de92169af2ae3bf90edf6268afd4e7b5
ochieng odock <odokclement>
2024-11-28 11:33:00 +0300

    classes in js

--- index.js
+++ index.js
@@ -136,3 +136,54 @@ man.lastName = "Bob";
 console.log(man);
 
 console.log(man.greet());
+
+// classes  ES6
+class Car{
+   constructor(name){
+      this.name = name;
+   }
+   // methods in class are stored in their prototype
+   getName(){
+      return`hello  ${this.name}`;
+   }
+   // static key word
+   //porperties and methods created using the static key are not placed on the prototype chain
+   // they are attached to the class function object i.e Car
+   // they dont reference any of the fields
+   static residence = "Earth";
+
+   static  greet(){
+      return "good morning";
+   }
+}
+
+console.log(Car.residence);
+console.log(Car.greet());
+
+// extends
+class BMW extends Car{
+   getClean(){
+     return `clean car`;
+   }
+}
+
+let GLE = new BMW("mercedes");
+console.log(GLE.getName());
+console.log(GLE.getClean());
+
+// private fields and methods use # can not be accessed directly from out side class function object
+
+//super refers parent class
+class Model extends Car{
+   constructor(name,model){
+      super(name);
+      this.model = model;
+   }
+   getNameModel(){
+      return `${this.getName()}   ${this.model}`;
+   }
+}
+
+let Mycar = new Model("BMW","M4");
+console.log(Mycar.getNameModel());
+
